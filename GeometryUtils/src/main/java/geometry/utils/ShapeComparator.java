6cfc7291bce025ae04f9c4b47b723d9943d09124
Nastya B <2murka2@gmail.com>
2025-06-29 20:10:48 +0300

    add comparator

---
+++ GeometryUtils/src/main/java/geometry/utils/ShapeComparator.java
@@ -0,0 +1,7 @@
+package geometry.utils;
+
+public class ShapeComparator {
+    public static Integer compareAreas(double area1, double area2) {
+        return Double.compare(area1, area2);
+    }
+}
