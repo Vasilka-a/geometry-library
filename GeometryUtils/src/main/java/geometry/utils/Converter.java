3a02e7eb5a52730bd8f49a011b438716a325836f
Nastya B <2murka2@gmail.com>
2025-06-29 19:51:07 +0300

    add new module and function

---
+++ GeometryUtils/src/main/java/geometry/utils/Converter.java
@@ -0,0 +1,11 @@
+package geometry.utils;
+
+public class Converter {
+    public static double metersToCentimeters(double meters) {
+        return meters * 100;
+    }
+
+    public static double centimetersToMeters(double centimeters) {
+        return centimeters / 100;
+    }
+}
